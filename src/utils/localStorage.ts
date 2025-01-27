56f87443b29896b53a068788da14c47aaab54465
tmfsol <tdm.tmfsol@gmail.com>
2025-01-27 22:29:06 +0700

    Updates

--- src/utils/localStorage.ts
+++
@@ -1,1 +0,0 @@
-export { default as localStorage } from 'react-secure-storage';
--- src/utils/tailwind.ts
+++
@@ -1,7 +0,0 @@
-import { withFluid } from '@fluid-tailwind/tailwind-merge';
-import clsx, { ClassValue } from 'clsx';
-import { extendTailwindMerge } from 'tailwind-merge';
-
-export const twMerge = (...inputs: ClassValue[]) => {
-	return extendTailwindMerge(withFluid)(clsx(...inputs));
-};
